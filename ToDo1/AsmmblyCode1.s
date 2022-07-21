ea26d13a982024092bbc7c5e89e1a79156ca8719
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2022-07-21 19:44:47 +0300

    Update AsmmblyCode1.s

--- ToDo1/AsmmblyCode1.s
+++ ToDo1/AsmmblyCode1.s
@@ -19,12 +19,12 @@ __Vectors
 		
 Reset_Handler
 
-	MOV R0, #4
-	ADD R2,R0, #1 ; R2 = x + 1
-	LSL R2, R2, #2 ;R2 = 4(x+1)
-	LSL R1, R0, #4 ;R1 = 16x
-	TST R0,#1
-	ADDEQ R3,R1,R2 ;R1 = 16x + 4(x+1)
+	MOV R0, #1	;The value of x -> R0=x
+	ADD R2,R0, #1	; R2 = x + 1
+	LSL R2, R2, #2	;R2 = 4(x+1)
+	LSL R1, R0, #4	;R1 = 16x
+	TST R0,#1	;Check if x odd or even
+	ADDEQ R2,R2,R1	;R3 = 16x + 4(x+1)
 	
 
 	ALIGN 
